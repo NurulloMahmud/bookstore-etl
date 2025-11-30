3c098013e23a060b3217cf26c8b3e6d56fd692df
NurulloMahmud <nurullomahmud@gmail.com>
2025-12-01 04:27:39 +0500

    fixing data source dirs

--- config/settings.py
+++ config/settings.py
@@ -2,15 +2,12 @@ from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA1_DIR = BASE_DIR / "data" / "DATA1"
-DATA2_DIR = BASE_DIR / "data" / "DATA2"
-DATA3_DIR = BASE_DIR / "data" / "DATA3"
 
 # conversion rate
 EUR_TO_USD = 1.2
 
 # data folders to process
-DATA_FOLDERS = [DATA1_DIR, DATA2_DIR, DATA3_DIR]
+DATA_FOLDERS = ["DATA1", "DATA2", "DATA3"]
 
 # output paths
 OUTPUT_DIR = 'output'
